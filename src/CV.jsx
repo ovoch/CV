397e913e1b5b54fe75d7b1e45e7ec518032f4120
ovoch <59253203+ovoch@users.noreply.github.com>
2024-12-22 14:09:09 +0300

    Update CV.jsx

--- src/CV.jsx
+++ src/CV.jsx
@@ -12,6 +12,7 @@ const Resume = React.forwardRef((props, ref) => {
         <a href="mailto:a.schmailo2015@gmail.com">a.schmailo2015@gmail.com</a> 
       </p>
 
+      
       <section>
         <h3>Опыт работы</h3>
         
@@ -52,20 +53,20 @@ const Resume = React.forwardRef((props, ref) => {
         <p>
           Проект интернет магазина с фильтрами, поиском. При написании использовал React JS, TailwindCSS. 
           <br />
-          <a href="https://github.com/ovoch/Pet-projects/tree/main/lamoda" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
+          <a href="https://github.com/yourusername/lamoda" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
         </p>
         <h4>Notion</h4>
         <p>
           Проект с созданием заметок и регистрацией, которые отправляются на сервер. При создании использовал 
           React JS, json-server, redux, tailwindCSS.
           <br />
-          <a href="https://github.com/ovoch/Pet-projects/tree/main/redux" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
+          <a href="https://github.com/yourusername/notion" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
         </p>
         <h4>To do list</h4>
         <p>
           Проект списка сделанных заданий с использованием React JS.
           <br />
-          <a href="https://github.com/ovoch/Pet-projects/tree/main/to%20do%20list" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
+          <a href="https://github.com/yourusername/todo-list" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
         </p>
       </section>
     </div>
@@ -85,7 +86,7 @@ const App = () => {
       jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
     };
 
-    
+    // Используем html2pdf для сохранения PDF
     html2pdf()
       .from(element)
       .set(options)
@@ -113,3 +114,5 @@ const App = () => {
     </div>
   );
 };
+
+export default App;
