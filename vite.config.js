64b2e1f0c48650f432768077d4ee816e62134b29
ovoch <59253203+ovoch@users.noreply.github.com>
2024-12-22 12:59:28 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ovoch.gitlab.io/CV"
+  base: "https://ovoch.gitlab.io/CV",
 })
